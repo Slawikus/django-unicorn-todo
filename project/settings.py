c9acd1bb0929958b1e5dfa30d77abf4354a38e2e
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-03-09 22:23:44 +0100

    Add boostrap to templates

--- project/settings.py
+++ project/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BOOTSTRAP4 = {
+    # Include jQuery with Bootstrap JavaScript False|falsy|slim|full (default=False)
+    # False - means tag bootstrap_javascript use default value - `falsy` and does not include jQuery)
+    'include_jquery': 'full',
+}
