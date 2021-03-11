227579e8ddf32f2d8a0155f57cecb68f031141ae
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-03-11 23:33:38 +0100

    Initial setup for Django Unicorn

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    "django_unicorn",
     'todos',
 ]
 
--- project/urls.py
+++ project/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('todos.urls')),
+    path("unicorn/", include("django_unicorn.urls")),
 ]
