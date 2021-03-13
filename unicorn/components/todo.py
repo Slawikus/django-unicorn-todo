1f839e13756654ff5f91904a167bfe47474048a6
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-03-13 19:25:38 +0100

    Extract one todo into dedicated component

---
+++ unicorn/components/todo.py
@@ -0,0 +1,13 @@
+from django_unicorn.components import UnicornView
+
+
+class TodoView(UnicornView):
+    todo = None
+
+    def delete(self):
+        self.todo.delete()
+        self.parent.load_todos()
+
+    def edit(self):
+        print('edit')
+        return True
