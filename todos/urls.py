c7e88a3f07397dd2b5035c047594a3c3ea490cb1
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-03-14 22:21:38 +0100

    Remove unnecessary views and templates

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from todos import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.TodoListView.as_view(), name='todo_list'),
-    path('new/', views.TodoCreateView.as_view(), name='todo_new'),
-    path('<int:pk>/edit/', views.TodoUpdateView.as_view(), name='todo_update'),
-    path('<int:pk>/delete/', views.TodoDeleteView.as_view(), name='todo_delete'),
+    path('', TemplateView.as_view(template_name='todos/todo_list.html')),
 ]
