1ab0cd42cbd9c1c70d9c4742cfa67e97db1dd845
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-03-14 22:28:16 +0100

    Simplify AddTodo component

--- unicorn/components/add_todo.py
+++ unicorn/components/add_todo.py
@@ -1,20 +1,13 @@
 from django_unicorn.components import UnicornView
 
-from todos.forms import TodoCreateForm
 from todos.models import Todo
 
 
 class AddTodoView(UnicornView):
-    form_class = TodoCreateForm
-
     description = None
-    due_date = None
 
     def add(self):
-        if not self.is_valid():
-            return self.validate()
-
-        todo = Todo.objects.create(description=self.description, due_date=self.due_date)
+        todo = Todo.objects.create(description=self.description)
 
         self.parent.load_todos()
         self._reset_state()
@@ -23,4 +16,3 @@ class AddTodoView(UnicornView):
 
     def _reset_state(self):
         self.description = None
-        self.due_date = None
