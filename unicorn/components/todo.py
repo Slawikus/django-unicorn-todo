1967f777f457690bba841019541f16be1ae957b4
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2021-03-14 22:42:20 +0100

    Simplify Todos component

--- unicorn/components/todo.py
+++ unicorn/components/todo.py
@@ -3,18 +3,7 @@ from django_unicorn.components import UnicornView
 
 class TodoView(UnicornView):
     todo = None
-    is_editing = False
 
     def delete(self):
         self.todo.delete()
         self.parent.load_todos()
-
-    def toggle_edit(self):
-        self.is_editing = not self.is_editing
-
-        if self.is_editing:
-            self.call("setFocus")
-
-    def save(self):
-        self.todo.save()
-        self.is_editing = False
